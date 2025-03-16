0fe4efac4bbcc5177c768f2274743d89359da3e4
Tlanda40 <teddy.landa@gmail.com>
2025-03-15 21:25:47 -0400

    added class Matrix with constructor and string methods

---
+++ LinAlgLib.py
@@ -0,0 +1,15 @@
+class Matrix:
+    def __init__(self, values):
+        self.values = values
+        self.rows = len(values)
+        self.cols = len(values[0])
+
+    def __str__(self):
+        string = ""
+        for i in range(self.rows):
+            for j in range(self.cols):
+                string = string + str(self.values[i][j]) + " "
+            string = string + "\n"
+        return string
+
+
